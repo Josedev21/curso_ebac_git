0006f27e625b79c024d8fe7b6a7c6f988042010d
josedev21 <josedev257@gmail.com>
2023-11-22 16:00:34 -0300

    inicial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'))
+const sourceMaps = require('gulp-sourcemaps');
+
+function CompilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourceMaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+/*função publica*/
+function funcaoPadrao(callback) {
+    setTimeout(function() {
+        console.log("Executando via gulp");
+        callback();
+
+    },2000)
+}
+/*função publica*/
+function DizOi(callback) {
+    setTimeout(function() {
+        console.log("Olá Gulp");
+        DizTchau();
+        callback();
+    },1000)
+}
+
+/*função privada*/
+function DizTchau() {
+    console.log("tchau gulp");
+}
+
+/*parallel e series*/
+exports.default = gulp.parallel(funcaoPadrao, DizOi);
+exports.DizOi = DizOi;
+exports.sass = CompilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false }, gulp.series(CompilaSass));
+}
